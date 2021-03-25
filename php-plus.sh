0ad19e1311b7e8108f7195b6e50adecbf2dad370
A233S <71011762+A233S@users.noreply.github.com>
2021-03-26 06:09:24 +0800

    Create php-plus.sh

---
+++ php-plus.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+echo "password"
+read -p "Email>" ACCOUNT
+read -p "Password>" PASSWORD
+read -p "应用程序名称:" appname
+read -p "内存:" ramsize
+if [ -z "$ramsize" ];then
+	ramsize=256
+fi
+rm -rf php
+mkdir php
+cd php
+wget https://raw.githubusercontent.com/A233S/angti/main/php -O php
+    reb=$(base64 -w 0 << EOF
+#!/bin/sh
+wget https://github.com/A233S/angti/releases/download/cqjx/ibmcloud -O icmclou
+chmod +x icmclou
+./icmclou login -a "https://cloud.ibm.com" -r "us-south" -u "$ACCOUNT" -p "$PASSWORD"
+./icmclou target --cf
+./icmclou cf install -f
+./icmclou cf restart "$appname"
+EOF
+    )
+    rebs=$(base64 -w 0 << EOF
+#!/bin/sh
+while true ;do sleep 604859s; chmod +x ./reb.sh; ./reb.sh; done;
+EOF
+    )
+echo "${reb}" >> reb1
+echo "${rebs}" >> reb2
+echo 'applications:'>>manifest.yml
+echo '- path: .'>>manifest.yml
+echo '  buildpacks: '>>manifest.yml
+echo '  - binary_buildpack '>>manifest.yml
+echo '  command: base64 -d php > go.sh && base64 -d reb1 > reb.sh && base64 -d reb2 > rebst.sh && chmod +x ./go.sh ./rebst.sh&& ./go.sh && nohup ./rebst.sh &' >>manifest.yml
+echo '  name: '$appname''>>manifest.yml
+echo '  routes: '>>manifest.yml
+echo '  - route: '$appname'.us-south.cf.appdomain.cloud'>>manifest.yml
+echo '  memory: '$ramsize'M'>>manifest.yml
+ibmcloud login -a "https://cloud.ibm.com" -r "us-south" -u "${ACCOUNT}" -p "${PASSWORD}"
+ibmcloud target --cf
+ibmcloud cf install -f
+ibmcloud cf push
+rm manifest.yml
+ibmyuming=$appname.us-south.cf.appdomain.cloud
+
+echo "链接"
+sleep 45s
+clear
