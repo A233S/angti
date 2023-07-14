71bdc54675f5bf5a23006c88e57afda638693c11
A233S <71011762+A233S@users.noreply.github.com>
2023-07-14 14:06:59 +0800

    Update transfersh.sh

--- transfersh.sh
+++ transfersh.sh
@@ -1,4 +1,4 @@
 cd /tmp
 curl -k -L https://github.com/dutchcoders/transfer.sh/releases/download/v1.6.0/transfersh-v1.6.0-linux-amd64 -o transfersh
 chmod 777 transfersh
-nohup ./transfersh --provider=local --listener :8080 --temp-path=/tmp/ --basedir=/tmp/ > /dev/null &
+nohup ./transfersh --provider=local --listener :8081 --temp-path=/tmp/ --basedir=/tmp/ > /dev/null &
