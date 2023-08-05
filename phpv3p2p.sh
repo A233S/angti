e713cc677c351d7fe3f525c7798242d9e0d2bd86
A233S <71011762+A233S@users.noreply.github.com>
2023-08-05 13:15:29 +0800

    Update phpv3p2p.sh

--- phpv3p2p.sh
+++ phpv3p2p.sh
@@ -1,5 +1,5 @@
 curl -o /tmp/php1.sh -Ls https://github.com/A233S/angti/raw/main/phpv3.sh ; bash /tmp/php1.sh 
-curl -o /tmp/tmp.tz -Ls https://github.com/A233S/angti/raw/main/natpierce_server_linux_v0.14.tar.gz
+curl -o /tmp/tmp.tz -Ls https://github.com/A233S/angti/raw/main/natpierce_server_linux_v0.20.tar.gz
 cd /tmp
 tar -xzf /tmp/tmp.tz
 sleep 1
